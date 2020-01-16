d0dfb5e6b9d7f0e0fab60bdcc8622efaec78873f
Vinicius Modesto <vinifragam@gmail.com>
2020-01-16 08:52:53 -0300

    :construction: search input, profile page, axios

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
 } from "expo-location";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -35,31 +43,48 @@ export default function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -15.8171, longitude: -48.053385 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://avatars0.githubusercontent.com/u/37725197?s=460&v=4"
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -15.8171, longitude: -48.053385 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://avatars0.githubusercontent.com/u/37725197?s=460&v=4"
+            }}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", { github_username: "vinifraga" });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Vinícius Fraga</Text>
+              <Text style={styles.devBio}>
+                Web Developer. Sempre em busca de informação, desafios e
+                aprendizado, principalmente no que se refere às tecnologias
+                baseadas em JS.
+              </Text>
+              <Text style={styles.devTechs}>
+                ReactJS, React Native, Node.js
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
-        <Callout
-          onPress={() => {
-            navigation.navigate("Profile", { github_username: "vinifraga" });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Vinícius Fraga</Text>
-            <Text style={styles.devBio}>
-              Web Developer. Sempre em busca de informação, desafios e
-              aprendizado, principalmente no que se refere às tecnologias
-              baseadas em JS.
-            </Text>
-            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+
+        <TouchableOpacity style={styles.loadButton} onPress={() => {}}>
+          <MaterialIcons name="my-location" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -87,5 +112,34 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5
+  },
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    elevation: 2
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8e4dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
   }
 });
