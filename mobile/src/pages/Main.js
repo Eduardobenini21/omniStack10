a47a1a55a7ffe46fc1562953e187494b46fdc0e8
Vinicius Modesto <vinifragam@gmail.com>
2020-01-17 07:28:09 -0300

    :construction: socket connection backend-mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -15,6 +15,7 @@ import {
 import { MaterialIcons } from "@expo/vector-icons";
 
 import api from "../services/api";
+import { connect, disconnect } from "../services/socket";
 
 export default function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -42,6 +43,12 @@ export default function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  function setupWebsocket() {
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -54,6 +61,7 @@ export default function Main({ navigation }) {
     });
 
     setDevs(response.data);
+    setupWebsocket();
   }
 
   function handleRegionChanged(region) {
